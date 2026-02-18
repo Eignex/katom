3a5e2d921cd9902deef2c6a5436745ac0ca7f80d
Rasmus Ros <rasmus.ros@theca.com>
2026-02-18 13:39:08 +0100

    feat: add DefaultStreamMode with thread-local handling and temporary mode override function

---
+++ src/main/kotlin/com/eignex/katom/DefaultStreamMode.kt
@@ -0,0 +1,20 @@
+package com.eignex.katom
+
+private val threadLocalMode = ThreadLocal.withInitial<StreamMode> { SerialMode }
+
+var defaultStreamMode: StreamMode
+    get() = threadLocalMode.get()
+    set(value) = threadLocalMode.set(value)
+
+/**
+ * Temporarily overrides the default mode for the duration of the block.
+ */
+fun <T> withMode(mode: StreamMode, block: () -> T): T {
+    val previousMode = defaultStreamMode
+    defaultStreamMode = mode
+    return try {
+        block()
+    } finally {
+        defaultStreamMode = previousMode
+    }
+}
