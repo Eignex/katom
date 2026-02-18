878a89b22db96739898727f6292c5e0bc17e9491
Rasmus Ros <rasmus.ros@theca.com>
2026-02-18 13:39:15 +0100

    refactor: restructure Result serialization and flattening logic

--- src/main/kotlin/com/eignex/katom/MapResult.kt
+++ src/main/kotlin/com/eignex/katom/MapResult.kt
@@ -1,63 +1,64 @@
 package com.eignex.katom
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.builtins.ArraySerializer
-import kotlinx.serialization.builtins.DoubleArraySerializer
-import kotlinx.serialization.builtins.MapSerializer
-import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.encoding.CompositeEncoder
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 
-@Serializable(with = MapResultSerializer::class)
-data class MapResult(val metrics: Map<String, Any>)
+@Serializable
+data class ResultEntry(
+    val name: String,
+    val value: ResultValue
+)
 
-object MapResultSerializer : KSerializer<MapResult> {
-
-    override val descriptor: SerialDescriptor = MapSerializer(String.serializer(), String.serializer()).descriptor
-
-    override fun serialize(encoder: Encoder, value: MapResult) {
-        val composite = encoder.beginStructure(descriptor)
-        var index = 0
-        value.metrics.forEach { (key, bit) ->
-            composite.encodeStringElement(descriptor, index++, key)
-            serializeValue(composite, index++, bit)
-        }
-        composite.endStructure(descriptor)
-    }
-
-    @OptIn(ExperimentalSerializationApi::class)
-    private fun serializeValue(composite: CompositeEncoder, index: Int, value: Any) {
-        when (value) {
-            is Double -> composite.encodeDoubleElement(descriptor, index, value)
-            is Long -> composite.encodeLongElement(descriptor, index, value)
-            is Int -> composite.encodeIntElement(descriptor, index, value)
-            is DoubleArray -> composite.encodeSerializableElement(descriptor, index, DoubleArraySerializer(), value)
-            is Array<*> -> {
-                @Suppress("UNCHECKED_CAST")
-                composite.encodeSerializableElement(descriptor, index, ArraySerializer(DoubleArraySerializer()), value as Array<DoubleArray>)
+@Serializable
+sealed interface ResultValue {
+    companion object {
+        fun create(value: Any): ResultValue {
+            return when {
+                value is Double -> ResultDouble(value)
+                value is DoubleArray -> ResultArray(value)
+                else -> throw IllegalArgumentException("Unsupported type for ResultValue: ${value::class.simpleName}")
             }
-            else -> throw UnsupportedOperationException("$value")
         }
     }
-
-    override fun deserialize(decoder: Decoder): MapResult =
-        throw UnsupportedOperationException("Deserialization not supported for MapResult.")
 }
 
-fun Result.toMap(prefixResults: Boolean = true): MapResult =
-    MapResult(this.flattenToMap(prefixResults))
+@Serializable
+@JvmInline
+value class ResultDouble(val value: Double) : ResultValue
 
-private fun Result.flattenToMap(
+@Serializable
+@JvmInline
+value class ResultArray(val values: DoubleArray) : ResultValue
+
+
+object ResultValueSerializer : KSerializer<ResultEntry> {
+
+    override fun deserialize(decoder: Decoder): ResultEntry =
+        throw UnsupportedOperationException("Deserialization not supported for MapResult.")
+
+    override val descriptor: SerialDescriptor
+        get() = TODO("Not yet implemented")
+
+    override fun serialize(
+        encoder: Encoder,
+        value: ResultEntry
+    ) {
+        TODO("Not yet implemented")
+    }
+}
+
+fun Result.flatten(prefixResults: Boolean = true): List<ResultEntry> =
+    flattenToList(prefixResults)
+
+private fun Result.flattenToList(
     usePrefix: Boolean,
     prefix: String = ""
-): Map<String, Any> {
+): List<ResultEntry> {
     val constructorOrder = this::class.primaryConstructor?.parameters
         ?.mapIndexed { index, param -> param.name to index }
         ?.toMap() ?: emptyMap()
@@ -66,17 +67,20 @@ private fun Result.flattenToMap(
         .filter { it.name in constructorOrder && it.name != "name" }
         .sortedBy { constructorOrder[it.name] }
 
-    val destination = mutableMapOf<String, Any>()
+    val destination = mutableListOf<ResultEntry>()
 
     sortedProperties.forEach { prop ->
         val value = prop.call(this) ?: return@forEach
         val key = if (usePrefix) "$prefix${prop.name}" else prop.name
 
+        // todo somehow result2 first/second not added
+        // trying to remove trim excess a bit
         if (value is Result) {
-            val newPrefix = if (usePrefix) "$prefix${value.name}." else ""
-            destination.putAll(value.flattenToMap(usePrefix, newPrefix))
+            val name = value.name ?: value::class.simpleName!!.removeSuffix("Result")
+            val newPrefix = if (usePrefix) "$prefix$name." else ""
+            destination.addAll(value.flattenToList(usePrefix, newPrefix))
         } else {
-            destination[key] = value
+            destination.add(ResultEntry(key, ResultValue.create(value)))
         }
     }
     return destination
